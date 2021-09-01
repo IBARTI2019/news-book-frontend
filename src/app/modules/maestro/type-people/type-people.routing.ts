7a74450f67655fa4053502b16ad52c419d258b71
Hemny Sibrian <sibrianhemny@gmail.com>
2021-09-01 05:09:46 -0400

    Applied permissions to type people routing.

--- src/app/modules/maestro/type-people/type-people.routing.ts
+++ src/app/modules/maestro/type-people/type-people.routing.ts
@@ -1,5 +1,7 @@
 import { _MatTabLinkBase } from "@angular/material/tabs";
 import { Routes } from "@angular/router";
+import { ADMIN } from 'app/constants';
+import { NgxPermissionsGuard } from 'ngx-permissions';
 import { CreateAndEditVehicleComponent } from "../vehicle/create-and-edit-vehicle/create-and-edit-vehicle.component";
 import { CreateAndEditTypePeopleComponent } from "./create-and-edit-type-people/create-and-edit-type-people.component";
 import { TypePeopleComponent } from "./type-people.component";
@@ -10,17 +12,26 @@ export const TypePeopleRouting: Routes = [
     children: [
       {
         path: "",
+        canActivate: [NgxPermissionsGuard],
         data: {
+          permissions: {
+            only: [ADMIN],
+            redirectTo: "/",
+          },
           breadcrumb: {
             label: "Tipoe de Personas",
           },
-          skipPermission: true,
         },
         component: TypePeopleComponent,
       },
       {
         path: "crear",
+        canActivate: [NgxPermissionsGuard],
         data: {
+          permissions: {
+            only: [ADMIN],
+            redirectTo: "/",
+          },
           breadcrumbAnt: {
             label: "Tipo de Personas",
             url: "type-people",
@@ -29,13 +40,17 @@ export const TypePeopleRouting: Routes = [
           breadcrumb: {
             label: "Crear Tipo",
           },
-          skipPermission: true,
         },
         component: CreateAndEditTypePeopleComponent,
       },
       {
         path: ":id",
+        canActivate: [NgxPermissionsGuard],
         data: {
+          permissions: {
+            only: [ADMIN],
+            redirectTo: "/",
+          },
           breadcrumbAnt: {
             label: "Tipo de Personas",
             url: "type-people",
